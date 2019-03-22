1413c991cb6ba994f95bed3ff8b5e65af7f34b7b
persja40 <persja40@gmail.com>
2019-03-22 11:47:48 +0100

    odczyt param i wypisanie na ekran

--- sensor.py
+++ sensor.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 
-print("DZIAŁA")
+import psutil as psu
+import time
+
+while(True):
+    cpu = psu.cpu_percent()
+    ram = psu.virtual_memory()
+    print("CPU {}%".format(cpu))
+    print("RAM total: {}GB \t used: {}GB {}%".format(round(ram.total / pow(2,30), 2), round(ram.used / pow(2,30), 2), ram.percent))
+    time.sleep(1)
