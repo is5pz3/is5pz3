2309a3e7679559a39ed2a31222916283e9bcf7bd
persja40 <persja40@gmail.com>
2019-05-07 21:38:45 +0200

    sensors cpu & ram

---
+++ sensor_ram.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import psutil
+import pause
+import requests
+import platform
+import argparse
+import socket
+import json
+from datetime import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', '-n', help="sensor name",
+                    type=str, required=False)
+parser.add_argument('--address', '-a', help="host address",
+                    type=str, required=True)
+parser.add_argument('--interval', '-i',
+                    help="inteval in seconds", type=float, default=1.0)
+args = parser.parse_args()
+
+init_data = {
+    "sensor_id" : args.name,
+    "host_name" : socket.gethostname(),
+    "platform" : platform.system() + " "+platform.release(),
+    "metric" : "RamUsage",
+    "unit" : "%"
+}
+r = requests.post(args.address, data=json.dumps(init_data))
+if(r.status_code == 200):
+    while(True):
+        timestamp = int(datetime.timestamp(datetime.now()))
+        msrmnt = {
+            "timestamp" : timestamp,
+            "value" : psutil.virtual_memory().percent
+        }
+        try:
+            requests.post(args.address, data=json.dumps(msrmnt))
+        except:
+            print("nobody cares")
+        pause.until(datetime.fromtimestamp(timestamp + args.interval))
+else:
+    print("NO CONNECTION")
