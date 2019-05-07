02983f7ed8c11d6f6764bee60abd7c841a5b110c
persja40 <persja40@gmail.com>
2019-05-07 20:36:01 +0200

    sensor args

--- sensor.py
+++ sensor.py
@@ -2,10 +2,31 @@
 
 import psutil as psu
 import time
+import requests
+import platform
+import argparse
+import socket
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--address', '-a', help="host address",
+                    type=str, required=True)
+parser.add_argument('--interval', '-i',
+                    help="inteval in seconds", type=float, default=1.0)
+args = parser.parse_args()
 
 while(True):
     cpu = psu.cpu_percent()
     ram = psu.virtual_memory()
+    print(socket.gethostname())
+    print(args.address)
+    print(platform.system() + " "+platform.release())
     print("CPU {}%".format(cpu))
-    print("RAM total: {}GB \t used: {}GB {}%".format(round(ram.total / pow(2,30), 2), round(ram.used / pow(2,30), 2), ram.percent))
-    time.sleep(1)
+    print("RAM total: {}GB \t used: {}GB {}%".format(
+        round(ram.total / pow(2, 30), 2), round(ram.used / pow(2, 30), 2), ram.percent))
+    msrmnt = {}
+    try:
+        requests.post(args.address, data=json.dumps(msrmnt))
+    except:
+        print("nobody cares")
+    time.sleep(args.interval)
